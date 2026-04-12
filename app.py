0ad1d51c07d4c30cf0f7b1ddba508059833a0f98
JK56788 <josephegbejule3@gmail.com>
2026-04-13 00:50:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ from recommender import (
 )
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "https://anime-recommender-system-p1m6.onrender.com"
+            ]
+        }
+    }
+)
 
 # -----------------------------
 # GLOBAL CACHE SETTINGS
